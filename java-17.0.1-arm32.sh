81ba5cd2ccce0f75d3e68bb51d6597c26bcc8727
LightNabz <87846302+LightNabz@users.noreply.github.com>
2021-11-30 21:47:09 +0800

    Update java-17.0.1-arm32.sh

--- java-17.0.1-arm32.sh
+++ java-17.0.1-arm32.sh
@@ -3,15 +3,20 @@
 #PLEASE DONT RECODE THIS BASH FILE :(
 
 #credit
-echo -e "\e[34m################################"
-echo -e "\e[34m#                              #"
-echo -e "\e[34m#   SCRIPT MADE BY LIGHTNABZ   #"
-echo -e "\e[34m#                              #"
-echo -e "\e[34m################################"
+echo -e "\e[34m##################################################"
+echo -e "\e[34m#                                                #"
+echo -e "\e[34m#                                                #"
+echo -e "\e[34m#              MADE BY LIGHNABZ                  #"
+echo -e "\e[34m#                                                #"
+echo -e "\e[34m#                                                #"
+echo -e "\e[34m##################################################"
+
 echo ""
 echo -e "\e[91mNOTE:"
 echo -e "\e[33mJava file source taken from \e[96mBellSoft Liberica"
-echo -e "\e[33mThis script only work on cpu with ARM 32bit architecture"
+dpkg --print-architecture
+read cpuarch
+echo -e "\e[33mYour arm architecture is $cpuarch"
 echo -e "\e[33mPlease dont recode this script"
 echo ""
 echo -e "\e[92mJoin To Our Discord!"
@@ -29,10 +34,20 @@ echo ""
 #start installing java tgz files
 echo -e "\e[33mInstalling and setup Java..."
 echo -e "\e[33mThis may take a few moments, you maybe can grab a coffe :)..."
-wget --no-check-certificate https://download.bell-sw.com/java/17.0.1+12/bellsoft-jdk17.0.1+12-linux-arm32-vfp-hflt.tar.gz &> /dev/null
+case `dpkg --print-architecture` in
+arm64)
+      archurl="aarch64" ;;
+aarch64)
+      archurl="aarch64" ;;
+armhf)
+      archurl="arm32-vfp-hflt" ;;
+arm)
+      archurl="arm32-vfp-hflt" ;;
+
+wget --no-check-certificate https://download.bell-sw.com/java/17.0.1+12/bellsoft-jdk17.0.1+12-linux-{$archurl}.tar.gz &> /dev/null
 
 #start set java for dpkg
-tar -xvzf bellsoft-jdk17.0.1+12-linux-arm32-vfp-hflt.tar.gz &> /dev/null
+tar -xvzf bellsoft-jdk17.0.1+12-linux-{$archurl}.tar.gz &> /dev/null
 mv ~/jdk-17.0.1 /usr/lib/jvm
 update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk-17.0.1/bin/java" 0 &> /dev/null
 update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk-17.0.1/bin/javac" 0 &> /dev/null
